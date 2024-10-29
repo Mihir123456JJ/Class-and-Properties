32d582d20b3298db1e0c5e00e8e7e0f297c2971f
Mihir123456JJ <kalothiamihir@gmail.com>
2024-10-29 19:31:58 +0530

    Objects

---
+++ main.py
@@ -0,0 +1,15 @@
+class students:
+    def _init_(self,name,grade,age):
+        self.name=name
+        self.grade=grade
+        self.age=age
+Stu1=students("Atharv","4th","10 years")
+Stu2=students("Mia","1st","5 years")
+Stu3=students("Bobby","11th","16 years")
+Stu4=students("Sara","4th","9 years")
+print("Bobby's name is",Stu3.name)
+print("Atharv is in",Stu1.grade)
+print("Mia's age is",Stu2.age)
+print("Sara is in",Stu4.grade)
+
+
